d757bbdf36715839ee8e6d775f93988bed37db70
VANTHONG <benjamin.vanthong@icloud.com>
2014-11-10 15:51:30 +0100

    debug euler.c

--- src/euler.c
+++ src/euler.c
@@ -1,4 +1,4 @@
-#include "euler.h"
+#include "Euler.h"
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
@@ -6,14 +6,14 @@
 #define ONE_OVER_SQRT_3 (0.57735026918962584)
 #define ONE_OVER_SQRT_2 (0.707106781186547524400844362105)
 
-const double euler_v[] = {
+const double Euler_v[] = {
   ONE_OVER_SQRT_3,
   ONE_OVER_SQRT_3,
   ONE_OVER_SQRT_3};
 
-//const double euler_v[] = {1,0,0};
+//const double Euler_v[] = {1,0,0};
 
-const double euler_v2d[] = {
+const double Euler_v2d[] = {
   ONE_OVER_SQRT_2,
   ONE_OVER_SQRT_2,
   0};
@@ -22,9 +22,9 @@ const double euler_v2d[] = {
 void EulerNumFlux(double wL[],double wR[],double* vnorm,double* flux){
   
   double vn =
-    euler_v[0] * vnorm[0] +
-    euler_v[1] * vnorm[1] +
-    euler_v[2] * vnorm[2];
+    Euler_v[0] * vnorm[0] +
+    Euler_v[1] * vnorm[1] +
+    Euler_v[2] * vnorm[2];
 
    double vnp = vn>0 ? vn : 0;
    double vnm = vn-vnp;
@@ -36,9 +36,9 @@ void EulerNumFlux(double wL[],double wR[],double* vnorm,double* flux){
 void EulerNumFlux2d(double wL[],double wR[],double* vnorm,double* flux){
   
   double vn =
-    euler_v2d[0] * vnorm[0] +
-    euler_v2d[1] * vnorm[1] +
-    euler_v2d[2] * vnorm[2];
+    Euler_v2d[0] * vnorm[0] +
+    Euler_v2d[1] * vnorm[1] +
+    Euler_v2d[2] * vnorm[2];
 
    double vnp = vn>0 ? vn : 0;
    double vnm = vn-vnp;
@@ -57,28 +57,28 @@ void EulerNumFlux2d(double wL[],double wR[],double* vnorm,double* flux){
 void EulerBoundaryFlux(double x[3],double t,double wL[],double* vnorm,
 			   double* flux){
   double wR[1];
-  eulerImposedData(x,t,wR);
-  eulerNumFlux(wL,wR,vnorm,flux);
+  EulerImposedData(x,t,wR);
+  EulerNumFlux(wL,wR,vnorm,flux);
 };
 
 void EulerBoundaryFlux2d(double x[3],double t,double wL[],double* vnorm,
 			   double* flux){
   double wR[1];
-  eulerImposedData2d(x,t,wR);
-  eulerNumFlux2d(wL,wR,vnorm,flux);
+  EulerImposedData2d(x,t,wR);
+  EulerNumFlux2d(wL,wR,vnorm,flux);
 };
 
 void EulerInitData(double x[3],double w[]){
 
   double t=0;
-  eulerImposedData(x,t,w);
+  EulerImposedData(x,t,w);
 
 };
 
 void EulerInitData2d(double x[3],double w[]){
 
   double t=0;
-  eulerImposedData2d(x,t,w);
+  EulerImposedData2d(x,t,w);
 
 };
 
@@ -86,9 +86,9 @@ void EulerInitData2d(double x[3],double w[]){
 void EulerImposedData(double x[3],double t,double w[]){
 
   double vx =
-    euler_v[0] * x[0] +
-    euler_v[1] * x[1] +
-    euler_v[2] * x[2];
+    Euler_v[0] * x[0] +
+    Euler_v[1] * x[1] +
+    Euler_v[2] * x[2];
 
   double xx = vx - t;
 
@@ -99,9 +99,9 @@ void EulerImposedData(double x[3],double t,double w[]){
 void EulerImposedData2d(double x[3],double t,double w[]){
 
   double vx =
-    euler_v2d[0] * x[0] +
-    euler_v2d[1] * x[1] +
-    euler_v2d[2] * x[2];
+    Euler_v2d[0] * x[0] +
+    Euler_v2d[1] * x[1] +
+    Euler_v2d[2] * x[2];
 
   double xx = vx - t;
 
@@ -112,21 +112,21 @@ void EulerImposedData2d(double x[3],double t,double w[]){
 void TestEulerBoundaryFlux(double x[3],double t,double wL[],double* vnorm,
 			   double* flux){
   double wR[1];
-  TesteulerImposedData(x,t,wR);
-  eulerNumFlux(wL,wR,vnorm,flux);
+  TestEulerImposedData(x,t,wR);
+  EulerNumFlux(wL,wR,vnorm,flux);
 };
 
 void TestEulerBoundaryFlux2d(double x[3],double t,double wL[],double* vnorm,
 			   double* flux){
   double wR[1];
-  TesteulerImposedData2d(x,t,wR);
-  eulerNumFlux2d(wL,wR,vnorm,flux);
+  TestEulerImposedData2d(x,t,wR);
+  EulerNumFlux2d(wL,wR,vnorm,flux);
 };
 
 void TestEulerInitData(double x[3],double w[]){
 
   double t=0;
-  TesteulerImposedData(x,t,w);
+  TestEulerImposedData(x,t,w);
 
 };
 
@@ -134,16 +134,16 @@ void TestEulerInitData(double x[3],double w[]){
 void TestEulerInitData2d(double x[3],double w[]){
 
   double t=0;
-  TesteulerImposedData2d(x,t,w);
+  TestEulerImposedData2d(x,t,w);
 
 };
 
 void TestEulerImposedData(double x[3],double t,double w[]){
 
   double vx =
-    euler_v[0] * x[0] +
-    euler_v[1] * x[1] +
-    euler_v[2] * x[2];
+    Euler_v[0] * x[0] +
+    Euler_v[1] * x[1] +
+    Euler_v[2] * x[2];
 
   double xx = vx - t;
 
@@ -154,9 +154,9 @@ void TestEulerImposedData(double x[3],double t,double w[]){
 void TestEulerImposedData2d(double x[3],double t,double w[]){
 
   double vx =
-    euler_v2d[0] * x[0] +
-    euler_v2d[1] * x[1] +
-    euler_v2d[2] * x[2];
+    Euler_v2d[0] * x[0] +
+    Euler_v2d[1] * x[1] +
+    Euler_v2d[2] * x[2];
 
   double xx = vx - t;
 
