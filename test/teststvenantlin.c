0a4f0005f31a3f41e3b90cc6afffbe77cde96343
MSPRENG <marion.spreng@etu.unistra.fr>
2014-12-31 17:07:48 +0100

    [teststvenantlin.c] use new cfl

--- test/teststvenantlin.c
+++ test/teststvenantlin.c
@@ -68,7 +68,7 @@ int TestStVenantLin(void){
   // apply the DG scheme
   // time integration by RK2 scheme 
   // up to final time = 1.
-  RK2(&f,0.5);//1.);
+  RK2StVenantLin(&f,0.5);//1.);
  
   // save the results and the error
   PlotField(0,(1==0),&f,"dgvisustvenantlin.msh");
