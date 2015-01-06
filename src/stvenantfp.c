e132a9d7170959804f20a56fae60f3334d6fe414
Mamadou Camara <camara0388@gmail.com>
2015-01-07 00:12:54 +0100

    model of stvenant flat bottom

--- src/stvenantfp.c
+++ src/stvenantfp.c
@@ -6,14 +6,14 @@
 #define ONE_OVER_SQRT_3 (0.57735026918962584)
 #define ONE_OVER_SQRT_2 (0.707106781186547524400844362105)
 
-const double transport_v[] = {
+const double stvnant_v[] = {
   ONE_OVER_SQRT_3,
   ONE_OVER_SQRT_3,
   ONE_OVER_SQRT_3};
 
 //const double transport_v[] = {1,0,0};
 
-const double transport_v2d[] = {
+const double stvenant_v2d[] = {
   ONE_OVER_SQRT_2,
   ONE_OVER_SQRT_2,
   0};
@@ -22,9 +22,9 @@ const double transport_v2d[] = {
 void StvenantFpNumFlux(double wL[],double wR[],double* vnorm,double* flux){
   
   double vn =
-    transport_v[0] * vnorm[0] +
-    transport_v[1] * vnorm[1] +
-    transport_v[2] * vnorm[2];
+    stvenant_v[0] * vnorm[0] +
+    stvenant_v[1] * vnorm[1] +
+    stvenant_v[2] * vnorm[2];
 
    double vnp = vn>0 ? vn : 0;
    double vnm = vn-vnp;
@@ -36,19 +36,15 @@ void StvenantFpNumFlux(double wL[],double wR[],double* vnorm,double* flux){
 void StvenantFpNumFlux2d(double wL[],double wR[],double* vnorm,double* flux){
   
   double vn =
-    transport_v2d[0] * vnorm[0] +
-    transport_v2d[1] * vnorm[1] +
-    transport_v2d[2] * vnorm[2];
+    stvenant_v2d[0] * vnorm[0] +
+    stvenant_v2d[1] * vnorm[1] +
+    stvenant_v2d[2] * vnorm[2];
 
    double vnp = vn>0 ? vn : 0;
    double vnm = vn-vnp;
 
    flux[0] = vnp * wL[0] + vnm * wR[0];
-   /* if (fabs(vnorm[2])>1e-6){ */
-   /*   printf("vnds %lf %lf %lf \n",vnorm[0],vnorm[1],vnorm[2]); */
-   /* } */
-   // verify that 2d computations are actually
-   // activated
+  
    assert(fabs(vnorm[2])<1e-8);
 
 
@@ -85,9 +81,9 @@ void StvenantFpInitData2d(double x[3],double w[]){
 void StvenantFpImposedData(double x[3],double t,double w[]){
 
   double vx =
-    transport_v[0] * x[0] +
-    transport_v[1] * x[1] +
-    transport_v[2] * x[2];
+    stvenant_v[0] * x[0] +
+    stvenant_v[1] * x[1] +
+    stvenant_v[2] * x[2];
 
   double xx = vx - t;
 
@@ -97,9 +93,9 @@ void StvenantFpImposedData(double x[3],double t,double w[]){
 void StvenantFpImposedData2d(double x[3],double t,double w[]){
 
   double vx =
-    transport_v2d[0] * x[0] +
-    transport_v2d[1] * x[1] +
-    transport_v2d[2] * x[2];
+    stvenant_v2d[0] * x[0] +
+    stvenant_v2d[1] * x[1] +
+    stvenant_v2d[2] * x[2];
 
   double xx = vx - t;
 
@@ -138,9 +134,9 @@ void TestStvenantFpInitData2d(double x[3],double w[]){
 void TestStvenantFpImposedData(double x[3],double t,double w[]){
 
   double vx =
-    transport_v[0] * x[0] +
-    transport_v[1] * x[1] +
-    transport_v[2] * x[2];
+    stvenant_v[0] * x[0] +
+    stvenant_v[1] * x[1] +
+    stvenant_v[2] * x[2];
 
   double xx = vx - t;
 
@@ -151,9 +147,9 @@ void TestStvenantFpImposedData(double x[3],double t,double w[]){
 void TestStvenantFpImposedData2d(double x[3],double t,double w[]){
 
   double vx =
-    transport_v2d[0] * x[0] +
-    transport_v2d[1] * x[1] +
-    transport_v2d[2] * x[2];
+    stvenant_v2d[0] * x[0] +
+    stvenant_v2d[1] * x[1] +
+    stvenant_v2d[2] * x[2];
 
   double xx = vx - t;
 
