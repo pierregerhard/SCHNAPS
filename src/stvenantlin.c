578ede9bfe13c5c814be14bf3ab924171d580915
MSPRENG <marion.spreng@etu.unistra.fr>
2015-01-08 17:51:13 +0100

    [stvenantlin.c] update

--- src/stvenantlin.c
+++ src/stvenantlin.c
@@ -42,22 +42,6 @@ void StVenantLinNumFlux2d(double wL[],double wR[],double* vnorm,double* flux){
 // rafraichissement vecteur normal
     vect_norm[0] = vnorm[0];
     vect_norm[1] = vnorm[1];
-  /*
-  double vn =
-    stvenantlin_v2d[0] * vnorm[0] +
-    stvenantlin_v2d[1] * vnorm[1] +
-    stvenantlin_v2d[2] * vnorm[2];
-
-   double vnp = vn>0 ? vn : 0;
-   double vnm = vn-vnp;
-
-  // flux[0] = vnp * wL[0] + vnm * wR[0];
-   flux[0]=0;
-   flux[1]=0;
-   flux[2]=0;
-
-*/
-
 
 //essai flux de Rusanov
 
@@ -139,22 +123,22 @@ void StVenantLinImposedData2d(double x[3],double t,double w[]){
 
 // onde de direction x
 // essai avec la solution exacte lorsqu'on prend la valeur propre sqrt(const_g H0)
-/*
+
 double val = sqrt(const_g*H0) ; 
 
 w[0] = (1/sqrt(const_g*H0))*(x[0]-val*t);
 w[1] = (x[0]-val*t);
 w[2] = 0 ; 
-*/
+
 
 // onde en biais
-
+/*
 double val = x[0]*(1) + x[1]*(-1) - sqrt(const_g*H0*(1))*t;
 
 w[0] = val ;
 w[1] = (vect_norm[0]*const_g*H0*val)/sqrt(const_g*H0*(1));
 w[2] = (vect_norm[1]*const_g*H0*val)/sqrt(const_g*H0*(1));
-
+*/
 };
 
 void TestStVenantLinBoundaryFlux(double x[3],double t,double wL[],double* vnorm,
